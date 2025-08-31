5dd930fbbda56f3c4e993d88a4fef0415d829a09
Oluwadarasimi <boatech4good@gmail.com>
2025-08-31 08:18:27 +0000

    wip

--- programs/dumpfun/src/constants/fees.rs
+++ programs/dumpfun/src/constants/fees.rs
@@ -1,5 +1,5 @@
 pub const TOKEN_INITIALISATION_FEE: u64 = 1_000_000_000 / 20; // 0.05 SOL
 
-pub const FEE_BPS: u64 = 100;
+pub const FEE_BPS: u64 = 100; // 1%
 
-pub const DEF_SLIPPAGE_BPS: u64 = 500;
+pub const DEF_SLIPPAGE_BPS: u64 = 50; // 0.5%
--- programs/dumpfun/src/constants/seeds.rs
+++ programs/dumpfun/src/constants/seeds.rs
@@ -1,7 +1,5 @@
 pub const BONDING_CURVE: &str = "bonding_curve";
 
-pub const SOL_RESERVE: &str = "sol_reserve";
-
 pub const GLOBAL_FEE_VAULT: &str = "global_fee_vault";
 
 pub const MINT_AUTHORITY: &str = "mint_authority";
